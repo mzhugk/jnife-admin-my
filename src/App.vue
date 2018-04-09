cd1e0f816c837ba5091d88e4031ef10b2f40dd0f
zgk <mzhugk@163.com>
2018-04-09 18:34:08 +0800

    22

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
     padding: 0;
     /*background: url(assets/bg1.jpg) center !important;
           background-size: cover;*/
-  // background: #1F2D3D;我改了master
+  // background: #1F2D3D;我改了master22222
     font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
     font-size: 14px;
     -webkit-font-smoothing: antialiased;

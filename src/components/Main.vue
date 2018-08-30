c742065de5154681656c2adc6ba49308d7bc20d0
zgk <mzhugk@163.com>
2018-08-30 15:02:54 +0800

    1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,24 @@
 <template>
 	<section>
-    <h1>Welcome back! {{user}}</h1>
+    <h1>Welcome back! {{user}}1</h1>
+    <div id='example-3'>
+      <input type="checkbox" id="jack" value="Jack1" v-model="checkedNames">
+      <label for="jack">Jack</label>
+      <input type="checkbox" id="john" value="John2" v-model="checkedNames">
+      <label for="john">John</label>
+      <input type="checkbox" id="mike" value="Mike3" v-model="checkedNames">
+      <label for="mike">Mike</label>
+      <br>
+      <span>Checked names: {{ checkedNames }}</span>
+    </div>
+    <input
+      id="d1"
+      type="checkbox"
+      v-model="toggle"
+      true-value="yes"
+      false-value="no"
+    >2323
+    <div>{{toggle}}</div>
 	</section>
 </template>
 
@@ -9,6 +27,8 @@
 	    data(){
 	        return{
 	            user:sessionStorage.getItem("user"),
+              checkedNames: ['Jack1','Mike3'],
+              toggle:''
           }
       }
 	}
